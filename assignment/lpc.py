12038d968531d4dd082fdba100d5d4ab1dda3ca1
JoseHung <1309716944@qq.com>
2022-09-30 14:44:36 +0800

    更新参数错误

--- assignment/lpc.py
+++ assignment/lpc.py
@@ -3,17 +3,17 @@ import numpy as np
 def lpc_coeff():
     # 这一部分是计算 auto-correlation parameters
     sig = np.array([1,2,6,2,6,7,2,8,3,4,6,7,8,5,8]) # 样本数据
-    auto_coeff = np.array([0,0,0,0]) # lpc参数长度
+    auto_coeff = np.array([0,0,0,0,0]) # lpc参数长度
     i = 0
-    while i < 4:
+    while i <= 4:
         j = i
         while j < 15:
             auto_coeff[i] += sig[j] * sig[j-i]
             j += 1
         i += 1
     print(auto_coeff)
-    a = np.array([[461,348,359], [348,461,348],[359,348,461]])
-    b = np.array([348,359,324])
+    a = np.array([[461,348,359,324], [348,461,348,359],[359,348,461,348],[324,359,348,461]])
+    b = np.array([348,359,324,254])
     inverse = np.linalg.inv(a)
     lpccoeff = np.dot(b, inverse)
     print(lpccoeff)
